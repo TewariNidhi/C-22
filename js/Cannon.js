88374bd005ff7ced3235b2d7338d3c0c637dae70
TewariNidhi <73495637+TewariNidhi@users.noreply.github.com>
2022-06-22 04:19:36 +0530

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,20 @@
+class Cannon{
+    constructor(x, y, w, h, angle){
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+        this.angle=angle;
+        this.cannon_base = loadImage("assets/cannonBase.png");
+        this.cannon_image = loadImage("assets/canon.png");
+    }
+
+    display(){
+        push()
+        imageMode(CENTER);
+        image(this.cannon_image,this.x, this.y, this.w, this.h);
+        pop();
+        image(this.cannon_base,70, 20, 200, 200);
+        noFill();
+    }
+}
